744a61b45f992b62c424e2f07ff1301bdaf71007
Ren4ik7284 <bebragnom89@gmail.com>
2026-06-12 15:24:52 +0300

    remove: delete data, dist and tsbuildinfo from git

--- dist/interfaces/url.interface.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IShortUrl {
-    id: number;
-    shortCode: string;
-    originalUrl: string;
-    clicks: number;
-    createdAt: Date;
-    updatedAt: Date;
-}
--- dist/url/url.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "sequelize-typescript";
-export declare class Url extends Model<Url> {
-    id: number;
-    shortCode: string;
-    originalUrl: string;
-    clicks: number;
-    createdAt: Date;
-    updatedAt: Date;
-}
